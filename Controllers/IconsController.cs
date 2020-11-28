aaf52f65508684af9e7145fe7dfae1a435ddf18d
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-28 21:21:53 +0100

    Adding the Icons Controller & View

---
+++ Controllers/IconsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialDashboardTheme.Controllers
+{
+    [Route("Icons")]
+    public class IconsController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
